397a04fca00403a60caa413b671eb735418f1a60
bhoover59 <77543359+bhoover59@users.noreply.github.com>
2023-02-17 10:23:14 -0500

    Delete shift_index.py

--- shift_index.py
+++
@@ -1,7 +0,0 @@
-def shift_index(df, column, shift_by):
-    df = df.copy()
-    shifted_index = (df.index + shift_by) % 24
-    df.index = shifted_index
-    df[column] = df[column].shift(shift_by)
-    df.loc[df.index[:shift_by], column] = df[column].iloc[-shift_by:].values
-    return df
